454cb01cbfe20164edb628d92fc34589ba0e94a3
Kimmo Forss <kimmo.forss@microsoft.com>
2020-07-10 00:25:15 +0300

    Revert "Delete variables_local.tf"
    This reverts commit f215ab6939ccf9dea869173f7146da6149823f58.

---
+++ deploy/terraform/modules/common_infrastructure/variables_local.tf
@@ -0,0 +1,43 @@
+variable "is_single_node_hana" {
+  description = "Checks if single node hana architecture scenario is being deployed"
+  default     = false
+}
+
+# Set defaults
+locals {
+  # Filter the list of databases to only HANA platform entries
+  hana-databases = [
+    for database in var.databases : database
+    if try(database.platform, "NONE") == "HANA"
+  ]
+  hdb    = try(local.hana-databases[0], {})
+  hdb_ha = try(local.hdb.high_availability, "false")
+  # If custom image is used, we do not overwrite os reference with default value
+  hdb_custom_image = try(local.hdb.os.source_image_id, "") != "" ? true : false
+  hdb_os = {
+    "source_image_id" = local.hdb_custom_image ? local.hdb.os.source_image_id : ""
+    "publisher"       = try(local.hdb.os.publisher, local.hdb_custom_image ? "" : "suse")
+    "offer"           = try(local.hdb.os.offer, local.hdb_custom_image ? "" : "sles-sap-12-sp5")
+    "sku"             = try(local.hdb.os.sku, local.hdb_custom_image ? "" : "gen1")
+  }
+
+  # iSCSI target device(s) is only created when below conditions met:
+  # - iscsi is defined in input JSON
+  # - AND
+  #   - HANA database has high_availability set to true
+  #   - HANA database uses SUSE
+  iscsi_count = lookup(var.infrastructure, "iscsi", {}) != {} && (length(local.hana-databases) > 0 ? (local.hdb_ha && upper(local.hdb_os.publisher) == "SUSE") : false) ? var.infrastructure.iscsi.iscsi_count : 0
+
+  # Shortcut to iSCSI definition
+  iscsi = merge(lookup(var.infrastructure, "iscsi", {}), { "iscsi_count" = "${local.iscsi_count}" })
+
+  # Shortcut to subnet block for iSCSI in input JSON
+  subnet_iscsi = merge({ "is_existing" = "false" }, lookup(var.infrastructure.vnets.sap, "subnet_iscsi", {}))
+
+  # The peering name can be max 80 characters
+  peeringNameTemp       = "${var.infrastructure.vnets.management.is_existing ? data.azurerm_virtual_network.vnet-management[0].resource_group_name : azurerm_virtual_network.vnet-management[0].resource_group_name}_${var.infrastructure.vnets.management.is_existing ? data.azurerm_virtual_network.vnet-management[0].name : azurerm_virtual_network.vnet-management[0].name}-${var.infrastructure.vnets.sap.is_existing ? data.azurerm_virtual_network.vnet-sap[0].resource_group_name : azurerm_virtual_network.vnet-sap[0].resource_group_name}_${var.infrastructure.vnets.sap.is_existing ? data.azurerm_virtual_network.vnet-sap[0].name : azurerm_virtual_network.vnet-sap[0].name}"
+  peeringNameSap        = length(local.peeringNameTemp) > 79 ? substr(local.peeringNameTemp, 0, 80) : local.peeringNameTemp
+  
+  peeringNameTempMgmt   = "${var.infrastructure.vnets.sap.is_existing ? data.azurerm_virtual_network.vnet-sap[0].resource_group_name : azurerm_virtual_network.vnet-sap[0].resource_group_name}_${var.infrastructure.vnets.sap.is_existing ? data.azurerm_virtual_network.vnet-sap[0].name : azurerm_virtual_network.vnet-sap[0].name}-${var.infrastructure.vnets.management.is_existing ? data.azurerm_virtual_network.vnet-management[0].resource_group_name : azurerm_virtual_network.vnet-management[0].resource_group_name}_${var.infrastructure.vnets.management.is_existing ? data.azurerm_virtual_network.vnet-management[0].name : azurerm_virtual_network.vnet-management[0].name}"
+  peeringNameManagement = length(local.peeringNameTempMgmt) > 79 ? substr(local.peeringNameTempMgmt, 0, 80) : local.peeringNameTempMgmt
+}
